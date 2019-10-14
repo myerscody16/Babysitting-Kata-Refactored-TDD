83ff0aaf5075306356fbaa1edd9bb8465a52e568
myerscody16 <myerscody16@live.com>
2019-10-13 21:13:16 -0400

    Family A and C calculations done with TDD

--- BabysittingKataRefactored/Controllers/HomeController.cs
+++ BabysittingKataRefactored/Controllers/HomeController.cs
@@ -17,19 +17,119 @@ namespace BabysittingKataRefactored.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(string startTime, string endTime)
+        public IActionResult Index(string startTime, string endTime, string familyLetter)
         {
-            DateTime start = Convert.ToDateTime(startTime);
-            DateTime end = Convert.ToDateTime(endTime);
+            DateTime start = DateTime.Parse(startTime);
+            DateTime end = DateTime.Parse(endTime);
             TimeSpan timeDifference = TimeDifference(start, end);
-            return RedirectToActionPermanent("Index");
+            int costOfAppointment = 0;
+            if(familyLetter == "A")
+            {
+                costOfAppointment = CalculateFamilyATotal(start, end);
+            }
+            else if (familyLetter == "B")
+            {
+                costOfAppointment = CalculateFamilyATotal(start, end);
+            }
+            else if (familyLetter == "C")
+            {
+                costOfAppointment = CalculateFamilyATotal(start, end);
+            }
+            return RedirectToAction("Index",costOfAppointment);
         }
         public TimeSpan TimeDifference(DateTime startTime, DateTime endTime)
         {
             TimeSpan differenceBetweenStartAndEndTime = endTime.Subtract(startTime);
             return differenceBetweenStartAndEndTime;
         }
+        public string FamilyLetterVerify(string familyLetter)
+        {
+            if(familyLetter == "A" || familyLetter == "B" || familyLetter == "C")
+            {
+                return familyLetter;
+            }
+            else
+            {
+                familyLetter = "Error";
+            }
+            return familyLetter;
+        }
+        public int CalculateFamilyATotal(DateTime start, DateTime end)
+        {
+            int total = 0;
+            DateTime firstCutOffTime = DateTime.Parse("2019-10-10T23:00");
+            if (start < firstCutOffTime && end > firstCutOffTime)
+            {
+                int differenceBetweenStartAndEndOfFirstCutOff = Convert.ToInt32(firstCutOffTime.Subtract(start).TotalHours);
+                if (differenceBetweenStartAndEndOfFirstCutOff < 0)
+                {
+                    differenceBetweenStartAndEndOfFirstCutOff = 0;
+                }
+                total += differenceBetweenStartAndEndOfFirstCutOff * 15;
+                int differenceBetweenEndTimeAndEndOfFirstCutOff = Convert.ToInt32(end.Subtract(firstCutOffTime).TotalHours);
+                if (differenceBetweenEndTimeAndEndOfFirstCutOff < 0)
+                {
+                    differenceBetweenEndTimeAndEndOfFirstCutOff = 0;
+                }
+                total += differenceBetweenEndTimeAndEndOfFirstCutOff * 20;
+            }
+            else if(start >= firstCutOffTime)
+            {
+                total += Convert.ToInt32(end.Subtract(start).TotalHours) * 20;
+            }
+            else if(start < end && end <= firstCutOffTime)
+            {
+                total += Convert.ToInt32(end.Subtract(start).TotalHours) * 15;
+            }
+            return total;
+        }
+        //public int CalculateFamilyBTotal(DateTime start, DateTime end)
+        //{
+        //    int total = 0;
+        //    int firstHourlyTotal = 0;
+        //    int secondHourlyTotal = 0;
+        //    DateTime firstCutOffTime = DateTime.Parse("2019-10-10T23:00Z");
+        //    DateTime secondCutOffTime = DateTime.Parse("2019-11-10T04:00Z");
+        //    if (start < firstCutOffTime && end > firstCutOffTime)
+        //    {
+        //        firstHourlyTotal = Convert.ToInt32(firstCutOffTime.Subtract(start).TotalHours);
+        //        secondHourlyTotal = Convert.ToInt32(end.Subtract(firstCutOffTime).TotalHours);
+        //        total += firstHourlyTotal * 15;
+        //        total += secondHourlyTotal * 20;
+        //    }
+        //    else if(start)
 
+        //    return total;
+        //}
+        public int CalculateFamilyCTotal(DateTime start, DateTime end)
+        {
+            int total = 0;
+            DateTime firstCutOffTime = DateTime.Parse("2019-10-10T23:00");
+            if (start < firstCutOffTime && end > firstCutOffTime)
+            {
+                int differenceBetweenStartAndFirstCutOff = Convert.ToInt32(firstCutOffTime.Subtract(start).TotalHours);
+                if (differenceBetweenStartAndFirstCutOff < 0)
+                {
+                    differenceBetweenStartAndFirstCutOff = 0;
+                }
+                total += differenceBetweenStartAndFirstCutOff * 21;
+                int differenceBetweenEndTimeAndFirstCutOff = Convert.ToInt32(end.Subtract(firstCutOffTime).TotalHours);
+                if (differenceBetweenEndTimeAndFirstCutOff < 0)
+                {
+                    differenceBetweenEndTimeAndFirstCutOff = 0;
+                }
+                total += differenceBetweenEndTimeAndFirstCutOff * 15;
+            }
+            else if (start >= firstCutOffTime)
+            {
+                total += Convert.ToInt32(end.Subtract(start).TotalHours) * 15;
+            }
+            else if (start < end && end <= firstCutOffTime)
+            {
+                total += Convert.ToInt32(end.Subtract(start).TotalHours) * 21;
+            }
+            return total;
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- NUnitTestProject1/HomeControllerTest.cs
+++ NUnitTestProject1/HomeControllerTest.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Tests
 {
-    public class Tests
+    public class InformationPassingTests
     {
 
         [SetUp]
@@ -47,5 +47,86 @@ namespace Tests
             TimeSpan testedValue = homeController.TimeDifference(start, end);
             Assert.AreEqual(differenceBetweenStartAndEndTime, testedValue);
         }
+        [TestCase("A")]
+        [TestCase("B")]
+        [TestCase("C")]
+        [Test]
+        public void TestFamilyChoice(string familyLetter)
+        {
+            var homeController = new HomeController();
+            string familyLetterVerified = homeController.FamilyLetterVerify(familyLetter);
+            Assert.AreEqual(familyLetter, familyLetterVerified);
+        }
+    }
+    public class CalculationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        //Family A Calculations
+        [TestCase("2019-10-10T17:00", "2019-10-11T04:00", ExpectedResult = 190)]
+        [TestCase("2019-10-10T19:00", "2019-10-11T00:00", ExpectedResult = 80)]
+        [Test]
+        public int FamilyATestCalculationWithStartBeforeCutOffAndEndAfterCutOff(DateTime start, DateTime end)
+        {
+            int total = 0;
+            var homeController = new HomeController();
+            total = homeController.CalculateFamilyATotal(start, end);
+            return total;
+        }
+        [TestCase("2019-10-10T23:00", "2019-10-11T04:00", ExpectedResult = 100)]
+        [TestCase("2019-10-11T00:00", "2019-10-11T03:00", ExpectedResult = 60)]
+        [Test]
+        public int FamilyATestCalculationWithStartAfterCutOffAndEndAfterCutOff(DateTime start, DateTime end)
+        {
+            int total = 0;
+            var homeController = new HomeController();
+            total = homeController.CalculateFamilyATotal(start, end);
+            return total;
+        }
+        [TestCase("2019-10-10T17:00", "2019-10-10T23:00", ExpectedResult = 90)]
+        [TestCase("2019-10-10T19:00", "2019-10-10T21:00", ExpectedResult = 30)]
+        [Test]
+        public int FamilyATestCalculationWithStartBeforeCutOffAndEndBeforeCutOff(DateTime start, DateTime end)
+        {
+            int total = 0;
+            var homeController = new HomeController();
+            total = homeController.CalculateFamilyATotal(start, end);
+            return total;
+        }
+        //Family C Calculations
+        [TestCase("2019-10-10T17:00", "2019-10-11T04:00", ExpectedResult = 126+75)]
+        [TestCase("2019-10-10T19:00", "2019-10-11T00:00", ExpectedResult = 84+15)]
+        [Test]
+        public int FamilyCTestCalculationWithStartBeforeCutOffAndEndAfterCutOff(DateTime start, DateTime end)
+        {
+            int total = 0;
+            var homeController = new HomeController();
+            total = homeController.CalculateFamilyCTotal(start, end);
+            return total;
+        }
+        [TestCase("2019-10-10T23:00", "2019-10-11T04:00", ExpectedResult = 75)]
+        [TestCase("2019-10-11T00:00", "2019-10-11T03:00", ExpectedResult = 45)]
+        [Test]
+        public int FamilyCTestCalculationWithStartAfterCutOffAndEndAfterCutOff(DateTime start, DateTime end)
+        {
+            int total = 0;
+            var homeController = new HomeController();
+            total = homeController.CalculateFamilyCTotal(start, end);
+            return total;
+        }
+        [TestCase("2019-10-10T17:00", "2019-10-10T23:00", ExpectedResult = 126)]
+        [TestCase("2019-10-10T19:00", "2019-10-10T21:00", ExpectedResult = 42)]
+        [Test]
+        public int FamilyCTestCalculationWithStartBeforeCutOffAndEndBeforeCutOff(DateTime start, DateTime end)
+        {
+            int total = 0;
+            var homeController = new HomeController();
+            total = homeController.CalculateFamilyCTotal(start, end);
+            return total;
+        }
+
     }
 }
